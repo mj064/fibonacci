c6e2a8ece5d49725a2ae01d0cdb048fe9a8ccecf
Mridul jain <mrujain064@gmail.com>
2026-02-26 22:24:30 +0530

    Update Fibonacci print format to comma-separated
    Change the output format of Fibonacci numbers to be comma-separated.

--- fibonacci.py
+++ fibonacci.py
@@ -4,10 +4,10 @@ def print_fibonacci(n):
     a, b = 0, 1
     count = 0
     while count < n:
-        print(a, end=' ')
+        print(a, end=',')
         a, b = b, a + b
         count += 1
-    print()  # for newline
+    print() 
 
 if __name__ == "__main__":
     try:
